d27dd90ee558ebeb67ae3b08265ad4a3cec7a165
tadeoo14 <tcardelli@frsn.utn.edu.ar>
2024-06-25 02:30:05 -0300

    Agregando-ingredientes

--- main.js
+++ main.js
@@ -32,10 +32,23 @@ function getDrink(box){
   .then(data =>{
     var drinkName = box.querySelector('h3');
     var drinkImg = box.querySelector('.card-img');
+    const drinkIngredients = box.querySelector('.drink-ingredients');
     drinkName.innerText = data.drinks[0].strDrink;
-    drinkImg.src = data.drinks[0].strDrinkThumb
-  });
+    drinkImg.src = data.drinks[0].strDrinkThumb;
+    console.log(data)
+    for (let i = 1; i <= 15; i++) {
+      if (data.drinks[0][`strIngredient${i}`] != null) {
+        var ingredient = document.createElement('p');
+        ingredient.innerText = data.drinks[0][`strIngredient${i}`] + " - " + data.drinks[0][`strMeasure${i}`];
+        drinkIngredients.appendChild(ingredient);
+      }
+    }
+    
+      
+  })
+
 }
+
 
 for (let i = 0; i < cards.length; i++) {
   getDrink(cards[i]);
