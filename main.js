dd68cbf605fdf84a499ccbeea926ae4cc8fe8562
Lucas Abratti <lucasabratti99@gmail.com>
2024-06-25 18:06:29 -0300

    Cards grid removed replaced by flex

--- main.js
+++ main.js
@@ -15,17 +15,17 @@ closeMenu.addEventListener("click", function() {
 let cards = document.getElementsByClassName("box");
 
 window.addEventListener("load", () =>{
-  var url = "http://colormind.io/api/";
-  var data = {
+  let url = "http://colormind.io/api/";
+  let data = {
     model : "default",
     input : [[ 19, 19, 20 ],"N","N","N","N"]
   }
   
-  var http = new XMLHttpRequest();
+  let http = new XMLHttpRequest();
   
   http.onreadystatechange = function() {
     if(http.readyState == 4 && http.status == 200) {
-      var palette = JSON.parse(http.responseText).result;
+      let palette = JSON.parse(http.responseText).result;
       document.documentElement.style.setProperty("--first-color", `rgb(${palette[0][0]},${palette[0][1]},${palette[0][2]})`);
       document.documentElement.style.setProperty("--second-color", `rgb(${palette[1][0]},${palette[1][1]},${palette[1][2]})`);
       document.documentElement.style.setProperty("--third-color", `rgb(${palette[2][0]},${palette[2][1]},${palette[2][2]})`);
@@ -39,21 +39,25 @@ window.addEventListener("load", () =>{
 });
 
 function getDrink(box){
-  var url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
   fetch(url)
   .then(response => response.json())
   .then(data =>{
-    var drinkName = box.querySelector('h3');
-    var drinkImg = box.querySelector('.card-img');
+    let drinkName = box.querySelector('h3');
+    let drinkImg = box.querySelector('.card-img');
     const drinkIngredients = box.querySelector('.drink-ingredients');
     drinkName.innerText = data.drinks[0].strDrink;
     drinkImg.src = data.drinks[0].strDrinkThumb;
     console.log(data)
     for (let i = 1; i <= 15; i++) {
       if (data.drinks[0][`strIngredient${i}`] != null) {
-        var ingredient = document.createElement('p');
-        ingredient.innerText = data.drinks[0][`strIngredient${i}`] + " - " + data.drinks[0][`strMeasure${i}`];
+        let ingredient = document.createElement('p');
+        let amount = data.drinks[0][`strMeasure${i}`];
+        ingredient.innerText = data.drinks[0][`strIngredient${i}`];
+        if(amount != null){
+          ingredient.innerText += " - " + data.drinks[0][`strMeasure${i}`]
+        }
         drinkIngredients.appendChild(ingredient);
       }
     }
@@ -62,7 +66,6 @@ function getDrink(box){
   })
 
 }
-
 
 for (let i = 0; i < cards.length; i++) {
   getDrink(cards[i]);
