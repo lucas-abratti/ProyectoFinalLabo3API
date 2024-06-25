72e2dfe27572e91cb1d3e66da7d679ad0406e119
Nachofrsn <icordero@frsn.utn.edu.ar>
2024-06-24 22:18:23 -0300

    added responsive design

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const openMenu = document.querySelector('.openMenu')
+const closeMenu = document.querySelector('.closeMenu')
+const menu = document.querySelector('.menu')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+openMenu.addEventListener("click", function() {
+  menu.style.display = 'flex'
+})
 
-setupCounter(document.querySelector('#counter'))
+closeMenu.addEventListener("click", function() {
+  menu.style.display = 'none'
+})
+
+
+
